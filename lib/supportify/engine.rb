ca1090b3fe5a67033c8f484c0a4f8f6273e22aa1
Yonah Forst <yonah.forst@gmail.com>
2014-09-10 11:33:21 +0200

    append migrations

--- lib/supportify/engine.rb
+++ lib/supportify/engine.rb
@@ -17,6 +17,14 @@ module Supportify
       app.config.assets.precompile += %w(supportify.css supportify.js)
     end
     
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+    
     initializer 'supportify.action_controller' do |app|
       ActiveSupport.on_load :action_controller do
         helper Supportify::ApplicationHelper
